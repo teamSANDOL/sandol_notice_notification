bc6ef3a72a109a2a62cf32a58f67a6781131fe4c
이원찬 <twoone14@tukorea.ac.kr>
2025-02-14 15:52:59 +0900

    Chore: dotenv, NODE_ENV 설정

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import indexRouter from "@/router";
+import dotenv from "dotenv";
 import express from "express";
 import morgan from "morgan";
+
+dotenv.config();
 
 const logger = morgan(
   ":method :url :status :res[content-length] - :response-time ms"
@@ -15,4 +18,5 @@ app.use("/", indexRouter);
 
 app.listen(3000, () => {
   console.log("server is running 3000");
+  console.log(`now NODE_ENV is ${process.env.NODE_ENV}`);
 });
